56d33eed6b9a8bd5fcd8708ed8144b896b60364b
Jose Luis Soto Sanchez <jose.soto.sa@usach.cl>
2023-12-12 18:49:22 -0300

    merge

--- OTEC-master/src/main/java/OTEC/OTEC/Controllers/Roles/RolController.java
+++ OTEC-master/src/main/java/OTEC/OTEC/Controllers/Roles/RolController.java
@@ -6,9 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rol")
+@RequestMapping("/roles")
 public class RolController {
 
     @Autowired
     private RolService rolService;
+
+
 }
