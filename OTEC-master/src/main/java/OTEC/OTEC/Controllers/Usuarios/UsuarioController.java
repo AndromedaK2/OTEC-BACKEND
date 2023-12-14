59e55a0ee968a1e008a7df5b75ab0687a4ed64d2
OrejiNet <63667148+OrejiNet@users.noreply.github.com>
2023-12-14 00:20:02 -0300

    add - usuarios

--- OTEC-master/src/main/java/OTEC/OTEC/Controllers/Usuarios/UsuarioController.java
+++ OTEC-master/src/main/java/OTEC/OTEC/Controllers/Usuarios/UsuarioController.java
@@ -1,15 +1,62 @@
 package OTEC.OTEC.Controllers.Usuarios;
 
+import OTEC.OTEC.Models.Usuarios.Usuario;
 import OTEC.OTEC.Services.Usuarios.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
+
+    @GetMapping(value = "/id/{id}")
+    public ResponseEntity<Usuario> obtenerRolPorId(@PathVariable("id") Integer idUsuario){
+        Usuario rol = usuarioService.findById(idUsuario);
+        return new ResponseEntity<>(rol, HttpStatus.OK);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obtenerUsuarios( ){
+        List<Usuario> usuarios = usuarioService.findAll();
+        return new ResponseEntity<>(usuarios,HttpStatus.OK);
+    }
+
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<Usuario>  registrar(@RequestBody Usuario usuario) {
+        Usuario usuarioCreado = usuarioService.register(usuario);
+        return new ResponseEntity<>(usuarioCreado,HttpStatus.CREATED);
+    }
+
+
+
+    @PostMapping("/login/{nombre}/{pass}")
+    public ResponseEntity<Usuario> login(@PathVariable("nombre") String nombre, @PathVariable("pass") String pass){
+        Usuario usuario =  usuarioService.login(nombre,pass);
+        return new ResponseEntity<>(usuario,HttpStatus.OK);
+    }
+
+
+    @PutMapping(value = "/actualizar")
+    public ResponseEntity<Usuario> actualizar(@RequestBody Usuario usuario){
+        Usuario user = usuarioService.update(usuario);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/id/{id}")
+    public ResponseEntity eliminarUsuarioPorId(@PathVariable("id") Integer idUser){
+        usuarioService.delete(idUser);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
 
 
 }
--- OTEC-master/src/main/java/OTEC/OTEC/Services/Usuarios/UsuarioService.java
+++ OTEC-master/src/main/java/OTEC/OTEC/Services/Usuarios/UsuarioService.java
@@ -3,7 +3,9 @@ package OTEC.OTEC.Services.Usuarios;
 import OTEC.OTEC.Models.Usuarios.Usuario;
 import OTEC.OTEC.Repositories.Usuarios.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,13 +20,26 @@ public class UsuarioService implements IUsuarioService<Usuario> {
     }
 
     @Override
-    public Optional<Usuario> findById(Integer id) {
-        return iUsuarioRepository.findById(id);
+    public Usuario findById(Integer id) {
+        Optional<Usuario> usuario = iUsuarioRepository.findById(id);
+        if(usuario.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("Usuario : %s no encontrado", id));
+        }
+        return usuario.get();
     }
 
     @Override
-    public Usuario create(Usuario usuario) {
+    public Usuario register(Usuario usuario) {
         return iUsuarioRepository.save(usuario);
+    }
+
+    @Override
+    public Usuario login(String nombre, String pass) {
+        Optional<Usuario> usuario =  iUsuarioRepository.login(nombre, pass);
+        if(usuario.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.CONFLICT, String.format("Usuario : %s no encontrado", nombre));
+        }
+        return usuario.get();
     }
 
     @Override
